c42922161f72f2bedc36d98f6a81bd2f8c2968a0
Daniel <nifemiolaniyi4@gmail.com>
2024-03-02 12:04:49 +0100

    ---

--- Djangoproject/myProject/myApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request): #new
-    return HttpResponse('<h1>Django Include URLs </h1>')
