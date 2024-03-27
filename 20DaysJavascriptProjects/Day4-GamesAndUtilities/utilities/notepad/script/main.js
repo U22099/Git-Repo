08878342702d1cb7fc3c4264e741fd581ad37d2d
Daniel <nifemiolaniyi4@gmail.com>
2024-03-27 20:02:53 +0100

    ---

--- 20DaysJavascriptProjects/Day4-GamesAndUtilities/utilities/notepad/script/main.js
+++ 20DaysJavascriptProjects/Day4-GamesAndUtilities/utilities/notepad/script/main.js
@@ -4,7 +4,19 @@ let textbox3;
 let textbox4;
 let textbox5;
 let textbox6;
+let destroy1;
+let destroy2;
+let destroy3;
+let destroy4;
+let destroy5;
+let destroy6;
 let no = 0;
+let int1;
+let int2;
+let int3;
+let int4;
+let int5;
+let int6;
 let input1 = "key1";
 let input2 = "key2";
 let input3 = "key3";
@@ -17,7 +29,6 @@ let state3 = "state3";
 let state4 = "state4";
 let state5 = "state5";
 let state6 = "state6";
-let noKeep = "noKeep";
 window.onload = ()=>{
     textbox1 = document.querySelector("#txt1");
     textbox2 = document.querySelector("#txt2");
@@ -25,22 +36,30 @@ window.onload = ()=>{
     textbox4 = document.querySelector("#txt4");
     textbox5 = document.querySelector("#txt5");
     textbox6 = document.querySelector("#txt6");
+    destroy1 = document.querySelector("#des1");
+    destroy2 = document.querySelector("#des2");
+    destroy3 = document.querySelector("#des3");
+    destroy4 = document.querySelector("#des4");
+    destroy5 = document.querySelector("#des5");
+    destroy6 = document.querySelector("#des6");
     textbox1.style.visibility = localStorage.getItem(state1);
     textbox2.style.visibility = localStorage.getItem(state2);
     textbox3.style.visibility = localStorage.getItem(state3);
     textbox4.style.visibility = localStorage.getItem(state4);
     textbox5.style.visibility = localStorage.getItem(state5);
     textbox6.style.visibility = localStorage.getItem(state6);
+    destroy1.style.visibility = localStorage.getItem(state1);
+    destroy2.style.visibility = localStorage.getItem(state2);
+    destroy3.style.visibility = localStorage.getItem(state3);
+    destroy4.style.visibility = localStorage.getItem(state4);
+    destroy5.style.visibility = localStorage.getItem(state5);
+    destroy6.style.visibility = localStorage.getItem(state6);
     textbox1.value = localStorage.getItem(input1);
     textbox2.value = localStorage.getItem(input2);
     textbox3.value = localStorage.getItem(input3);
     textbox4.value = localStorage.getItem(input4);
     textbox5.value = localStorage.getItem(input5);
     textbox6.value = localStorage.getItem(input6);
-    // no = localStorage.getItem(noKeep);
-    // if(no){
-    //     no += 1;
-    // }
 }
 function showNotepad(){
     let string;
@@ -49,7 +68,6 @@ function showNotepad(){
         no = 0;
     }
     no += 1;
-    localStorage.setItem(noKeep);
     if(no == 1){
         box1();
     }
@@ -70,62 +88,105 @@ function showNotepad(){
     }
 }
 function box1(){
+    destroy1.style.visibility = "visible";
     string = localStorage.getItem(input1);
     textbox1.style.visibility = "visible";
     textbox1.value = string;
     textbox1.focus()
-    setInterval(()=>{
+    int1 = setInterval(()=>{
         localStorage.setItem(input1, textbox1.value);
     }, 500);
     localStorage.setItem(state1, "visible");
 }
 function box2(){
+    destroy2.style.visibility = "visible";
     string = localStorage.getItem(input2);
     textbox2.style.visibility = "visible";
     textbox2.value = string;
     textbox2.focus()
-    setInterval(()=>{
+    int2 = setInterval(()=>{
         localStorage.setItem(input2, textbox2.value);
     }, 500);
     localStorage.setItem(state2, "visible");
 }
 function box3(){
+    destroy3.style.visibility = "visible";
     string = localStorage.getItem(input3);
     textbox3.style.visibility = "visible";
     textbox3.value = string;
     textbox3.focus()
-    setInterval(()=>{
+    int3 = setInterval(()=>{
         localStorage.setItem(input3, textbox3.value);
     }, 500);
     localStorage.setItem(state3, "visible");
 }
 function box4(){
+    destroy4.style.visibility = "visible";
     string = localStorage.getItem(input4);
     textbox4.style.visibility = "visible";
     textbox4.value = string;
     textbox4.focus()
-    setInterval(()=>{
+    int4 = setInterval(()=>{
         localStorage.setItem(input4, textbox4.value);
     }, 500);
     localStorage.setItem(state4, "visible");
 }
 function box5(){
+    destroy5.style.visibility = "visible";
     string = localStorage.getItem(input5);
     textbox5.style.visibility = "visible";
     textbox5.value = string;
     textbox5.focus()
-    setInterval(()=>{
+    int5 = setInterval(()=>{
         localStorage.setItem(input5, textbox5.value);
     }, 500);
     localStorage.setItem(state5, "visible");
 }
 function box6(){
+    destroy6.style.visibility = "visible";
     string = localStorage.getItem(input6);
     textbox6.style.visibility = "visible";
     textbox6.value = string;
     textbox6.focus()
-    setInterval(()=>{
+    int6 = setInterval(()=>{
         localStorage.setItem(input6, textbox6.value);
     }, 500);
     localStorage.setItem(state6, "visible");
+}
+function box1Remove(){
+    textbox1.style.visibility = "hidden";
+    destroy1.style.visibility = "hidden";
+    clearInterval(int1);
+    localStorage.clear(input1);
+    no = 0;
+}
+function box2Remove(){
+    textbox2.style.visibility = "hidden";
+    destroy2.style.visibility = "hidden";
+    localStorage.clear(input2);
+    no = 0;
+}
+function box3Remove(){
+    textbox3.style.visibility = "hidden";
+    destroy3.style.visibility = "hidden";
+    localStorage.clear(input3);
+    no = 0;
+}
+function box4Remove(){
+    textbox4.style.visibility = "hidden";
+    destroy4.style.visibility = "hidden";
+    localStorage.clear(input4);
+    no = 0;
+}
+function box5Remove(){
+    textbox5.style.visibility = "hidden";
+    destroy5.style.visibility = "hidden";
+    localStorage.clear(input5);
+    no = 0;
+}
+function box6Remove(){
+    textbox6.style.visibility = "hidden";
+    destroy6.style.visibility = "hidden";
+    localStorage.clear(input6);
+    no = 0;
 }
